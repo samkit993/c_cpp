061819448be04a9132666901a6c045c691aa9984
sam <samkit993@gmail.com>
2013-07-03 14:42:21 +0530

    added erdos_no

---
+++ erdos_no.cpp
@@ -0,0 +1,101 @@
+#include<map>
+#include<vector>
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int tCases;
+    ifstream instream("erdos_input.in");
+    if(instream.is_open()){
+        instream >> tCases;
+        //cout << tCases << endl;
+        instream.ignore();
+        vector<int> nPapers(tCases,0);
+        vector<int> nOutputs(tCases,0);
+
+
+        vector<string> temp_authors;
+        map<string, vector<string> > paper_dict;
+        map<string, int > authors_dict;
+        for(int p=0;p<tCases;p++){
+            instream >> nPapers[p] >> nOutputs[p];
+            //cout << nPapers[p] << "  " << nOutputs[p];
+            instream.ignore();
+            for(int q=0;q<nPapers[p];q++){
+                string temp="";
+                getline(instream, temp);
+                //cout << temp << endl;
+                int j=0;
+                bool check = false;
+                temp_authors.clear();
+                for(int i=0;i<temp.size();i++){
+                    if(temp[i]==','){
+                        if(check){
+                            string temp2="";
+                            for(int k=j;k<i;k++){
+                                temp2 += temp[k];
+                            }
+                            temp_authors.push_back(temp2);authors_dict[temp2]=999;
+                            j=i+2;
+                            check = false;
+                        } else {
+                            check = true;
+                        }
+                    }
+                    if(temp[i] == ':'){
+                        string temp2="";
+                        for(int k=j;k<i;k++){
+                            temp2 += temp[k];
+                        }
+                        temp_authors.push_back(temp2);authors_dict[temp2]=999;
+                        temp2="";
+                        for(int k=i+2;k<temp.size();++k){
+                            temp2 += temp[k];
+                        }
+                        paper_dict[temp2] = temp_authors;
+
+                    }
+
+                }
+            }
+            map<string,vector<string> >::iterator pos;
+            for(pos = paper_dict.begin();pos!=paper_dict.end();pos++){
+                if(pos->second.end()!= find(pos->second.begin(),pos->second.end(),"Erdos, P.")){
+                    for(int i=0; i< pos->second.size(); i++){
+                        authors_dict[pos->second[i]] = 1;
+                    }
+                }
+            }
+            for(pos = paper_dict.begin();pos!=paper_dict.end();pos++){
+                int minimum = 999;
+                vector<string>::iterator ii;
+                for(ii = pos->second.begin(); ii!=pos->second.end();ii++){
+                    if(authors_dict[*ii] < minimum){
+                        minimum = authors_dict[*ii];
+                    }
+                }
+
+                for(int i=0; i< pos->second.size(); i++){
+                    if(authors_dict[pos->second[i]]!=minimum && authors_dict[pos->second[i]] > minimum)
+                        authors_dict[pos->second[i]] = minimum+1;
+                }
+            }
+            //instream.ignore();
+            for(int r=0;r<nOutputs[p];++r){
+                string temp;
+                getline(instream, temp);
+                //cout << temp << endl;
+
+                if(authors_dict.end() == authors_dict.find(temp))
+                    authors_dict[temp] = 999;
+
+                if(authors_dict[temp]==999)
+                    cout << temp << " " << "infinity" << endl;
+                else
+                    cout << temp << " " << authors_dict[temp] << endl;
+            }
+        }
+    }
+    return 0;
+}
---
+++ lc_display.cpp
@@ -0,0 +1,83 @@
+#include<cstdio>
+#include<iostream>
+#include<string>
+
+/*
+ *PC/UVa IDs: 110104/706, Popularity: A, Success rate: average Level: 1
+    A friend of yours has just bought a new computer. Before this, the most powerful
+    machine he ever used was a pocket calculator. He is a little disappointed because he
+    liked the LCD display of his calculator more than the screen on his new computer! To
+    make him happy, write a program that prints numbers in LCD display style.
+    Input
+    The input file contains several lines, one for each number to be displayed. Each line
+    contains integers s and n, where n is the number to be displayed (0 ≤ n ≤ 99, 999, 999)
+    and s is the size in which it shall be displayed (1 ≤ s ≤ 10). The input will be terminated
+    by a line containing two zeros, which should not be processed.
+    Output
+    Print the numbers specified in the input file in an LCD display-style using s “-” signs
+    for the horizontal segments and s “|” signs for the vertical ones. Each digit occupies
+    exactly s + 2 columns and 2s + 3 rows. Be sure to fill all the white space occupied by
+    the digits with blanks, including the last digit. There must be exactly one column of
+    blanks between two digits.
+    Output a blank line after each number.
+
+ */
+using namespace std;
+int main(){
+    const char digitRep[10][7]=
+    {
+      '-','|','|',' ','|','|','-',
+      ' ',' ','|',' ',' ','|',' ',
+      '-',' ','|','-','|',' ','-',
+      '-',' ','|','-',' ','|','-',
+      ' ','|','|','-',' ','|',' ',
+      '-','|',' ','-',' ','|','-',
+      '-','|',' ','-','|','|','-',
+      '-',' ','|',' ',' ','|',' ',
+      '-','|','|','-','|','|','-',
+      '-','|','|','-',' ','|',' '
+    };
+    int s;
+    string number;
+    while(1){
+        cin >> s >> number;
+        if(s==0)
+            return 0;
+        for(int i=0;i<2*s+3;i++){
+            for(int j=0;j<number.size();j++){
+
+                int digit = number[j]-'0';
+
+                if(i%(s+1)==0){
+                    printf(" ");
+                    for(int k=0;k<s;k++){
+                        printf("%c",digitRep[digit][i/(s+1)*3]);
+                    }
+                    printf(" ");
+                }
+
+                if(i>0 && i<(s+1)){
+                    printf("%c",digitRep[digit][1]);
+                    for(int k=0;k<s;k++){
+                        printf(" ");
+                    }
+                    printf("%c",digitRep[digit][2]);
+                }
+
+                if(i>(s+1)&&i<(2*s+2)){
+                    printf("%c",digitRep[digit][4]);
+                    for(int k=0;k<s;k++){
+                        printf(" ");
+                    }
+                    printf("%c",digitRep[digit][5]);
+                }
+
+                if(j!=number.size()-1)
+                    printf(" ");
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    return 0;
+}
